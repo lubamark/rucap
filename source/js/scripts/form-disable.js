9464b307f756b8a3079ad44da781eb4ce3555167
lubamark <lubamark@mail.ru>
2018-09-07 16:48:05 +0700

    add script form disable

---
+++ source/js/scripts/form-disable.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var $form = $('.j-form');
+
+    $form.each(function () {
+        var $requiredCheckbox = $(this).find('.j-required-checkbox');
+        var $submitForm = $(this).find('.j-submit');
+        if( $requiredCheckbox.attr("checked") !== 'checked') {
+            $submitForm.addClass('_disabled');
+            $submitForm.prop('disabled', true);
+            $requiredCheckbox.on('change', function () {
+                $submitForm.toggleClass('_disabled');
+                $submitForm.prop('disabled', function (val) {
+                    return !val;
+                });
+            });
+        }
+    });
+});
